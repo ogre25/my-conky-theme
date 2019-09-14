45191233327824e2260b0561284d33d132f43da3
ogre25 <ogre25@126.com>
2019-09-14 22:24:19 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+read -p "Please input your appid  of openweather.org:" wapi
+read -p "Please input your city name(ex:newyork,london,shanghai):" cityname
+read -p "Please inpurt your country name(ex:us,en,cn):" countryname
+sed -i "s:your openweahter.org appid:$wapi:g" weather.py
+sed -i "s:city name:$cityname:g" weather.py
+sed -i "s:country name:$countryname:g" weather.py
 if [ -e $HOME/.font ];then
   cp ./fonts/* $HOME/.fonts
 else
